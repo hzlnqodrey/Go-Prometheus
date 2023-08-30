84b1f6b9eb1761f825a4819461b778ce4526a3f8
Hazlan Muhammad Qodri <byze.eternalground@gmail.com>
2023-08-31 00:26:59 +0700

    ping counter with prom http is working!

--- main.go
+++ main.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_request_count",
+		Help: "No of request handled by Ping handler",
+	},
 )
 
 func ping(w http.ResponseWriter, r *http.Request) {
+	pingCounter.Inc()
 	fmt.Fprintf(w, "pong")
 }
 
 func main() {
-	http.HandleFunc("/ping", ping)
+	prometheus.MustRegister(pingCounter)
 
+	http.HandleFunc("/ping", ping)
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":8090", nil)
 }
-
